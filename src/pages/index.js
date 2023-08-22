a58a24a4401599ac903469c8486f8d63015910c0
leoncan122 <leon.can122@gmail.com>
2023-08-22 11:48:54 +0200

    new element

--- src/pages/index.js
+++ src/pages/index.js
@@ -95,6 +95,21 @@ export default function Home() {
           link: 'https://developer.abnamro.com/api-products/confirmation-availability-funds-psd2/reference-documentation'
         }
       ]
+    },
+    {
+      category: 'PAYMENTS',
+      title: 'n8n TEST',
+      description: 'Retrieve confirmation on the availability of funds from an ABN AMRO account. ',
+      links: [
+        {
+          title: 'Overview',
+          link: 'https://developer.abnamro.com/api-products/confirmation-availability-funds-psd2api'
+        },
+        {
+          title: 'Documentation',
+          link: 'https://developer.abnamro.com/api-products/confirmation-availability-funds-psd2/reference-documentation'
+        }
+      ]
     }
   ]
   return (
